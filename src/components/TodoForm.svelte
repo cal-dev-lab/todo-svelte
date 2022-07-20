0cc5baec26abd803b3e8c8caaf09f88c93649166
Callum Lees <hi@c-l.dev>
2022-07-20 14:23:10 +0100

    Created TodoForm.svelte

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,17 @@
+<script>
+    import {addTodo} from "../stores/todoStore.js"
+    let todo = '';
+    const handleSubmit = () => {
+        addTodo(todo);
+        todo = '';
+        console.log('Submitting');
+    }
+</script>
+
+<form class="my-6" on:submit|preventDefault={handleSubmit}>
+    <div class="flex flex-col text-sm mb-2">
+        <label class="font-bold mb-2 text-gray-800" for="todo">Todo</label>
+        <input bind:value={todo} name="todo" placeholder="Add a todo" class="appearance-none shadow-sm border border-gray-200 p-2 focus:outline-none focus:border-gray-500 rounded-lg" />
+    </div>
+    <button type="submit" class="w-full shadow-sm rounded bg-green-500 hover:bg-green-400 text-white py-2 px-4">Add</button>
+</form>
