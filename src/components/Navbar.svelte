ecba67f5d4129b6c4901197afbeb84ec960bb569
Callum Lees <hi@c-l.dev>
2022-07-20 19:07:30 +0100

    Display user email when logged in

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -1,12 +1,14 @@
 <script>
     import { supabase } from "../supabase.js";
+    import { user } from "../stores/authStore.js";
 
     const logout = () => {
         supabase.auth.signOut();
     };
 </script>
 
-<div class="flex my-4 justify-end">
+<div class="flex my-4 justify-between items-center space-x-4">
+    <p class="text-white text-sm font-bold">{$user.email}</p>
     <button 
         class="shadow-sm rounded bg-blue-500 hover:bg-blue-600 text-white py-2 px-4"
         on:click={logout}
