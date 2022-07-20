3dad8f519e6b1a155c9547f4fdbed50fcb085de4
Callum Lees <hi@c-l.dev>
2022-07-20 13:50:16 +0100

    Imported TodoForm

---
+++ src/routes/index.svelte
@@ -0,0 +1,10 @@
+<script>
+    import TodoForm from "../components/TodoForm.svelte";
+</script>
+
+<main>
+    <h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">
+        Todo List
+    </h1>
+    <TodoForm />
+</main>
