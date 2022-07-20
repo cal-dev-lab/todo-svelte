8220bc32277217b74c5444e2d70673e4a59d307c
Callum Lees <hi@c-l.dev>
2022-07-20 18:52:37 +0100

    Added logic to determine todos to load on user

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,29 @@
 <script>
+    import { supabase } from "../supabase.js";
+    import { user } from "../stores/authStore.js";
+    import { loadTodos } from "../stores/todoStore.js";
+    import Auth from "../components/Auth.svelte";
+    import Navbar from "../components/Navbar.svelte";
     import "../app.css";
+    
+    
+
+    console.log(supabase.auth.user())
+    user.set(supabase.auth.user());
+
+    supabase.auth.onAuthStateChange((_, session) => {
+        user.set(session?.user)
+        if (session?.user) {
+            loadTodos();
+        }
+    }) 
 </script>
 
 <div class="container mx-auto my-6 max-w-2xl">
-    <slot></slot>
+    {#if $user}
+        <Navbar />
+        <slot></slot>
+    {:else}
+        <Auth />
+    {/if}
 </div>
